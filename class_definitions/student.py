5069da421004a28d418d149b5e74743be54d23f6
Reser, Jack C <jcreser@dmacc.edu>
2020-11-02 22:46:45 -0600

    updated constructor for 5th test. test passes

--- class_definitions/student.py
+++ class_definitions/student.py
@@ -8,6 +8,8 @@ class Student:
 
         if not self.last_name.isalpha():
             raise ValueError
+        if not self.first_name.isalpha():
+            raise ValueError
 
     def __str__(self):
         return self.last_name + ", " + self.first_name + " has major " + self.major + " with gpa: " + str(self.gpa)
